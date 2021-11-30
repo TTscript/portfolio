e5c296f3a2d2dc12441fa19fdd6d7921476a3172
Timur <t.tokumov@gmail.com>
2021-11-30 11:00:39 +0600

    fixed new-year4

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const allLang = ['en', 'ru'];
 const christmasTree = document.querySelector('.page-main__inner');
 
     christmasTree.addEventListener('click', () => {
-        window.location.pathname = '/new-year';
-        console.log('click');
+        if (window.location.pathname === '/portfolio/') {
+            window.location.pathname = '/portfolio/new-year';
+        } else {
+            window.location.pathname = '/new-year.html';
+        }
     });
 
 select.addEventListener('change', changeUrlLanguage)
@@ -35,33 +38,33 @@ function changeLanguage() {
         }
     }
 }
-
 changeLanguage();
 
-// if (window.location.pathname === '/new-year.html') {
-//     const days = document.querySelector('.main-new-year__days-num');
-//     const hours = document.querySelector('.main-new-year__hours-num');
-//     const minutes = document.querySelector('.main-new-year__minutes-num');
-//     const seconds = document.querySelector('.main-new-year__seconds-num');
+if (window.location.pathname === '/new-year.html') {
+    const days = document.querySelector('.main-new-year__days-num');
+    const hours = document.querySelector('.main-new-year__hours-num');
+    const minutes = document.querySelector('.main-new-year__minutes-num');
+    const seconds = document.querySelector('.main-new-year__seconds-num');
 
-//     const currentDate = new Date().getFullYear();
-//     const newYear = new Date(`January 1 ${currentDate + 1} 00:00:00`);
+    const currentDate = new Date().getFullYear();
+    const newYear = new Date(`January 1 ${currentDate + 1} 00:00:00`);
 
-//     function updateCountdownTime() {
-//         const currentTime = new Date();
-//         const diff = newYear - currentTime;
+    function updateCountdownTime() {
+        const currentTime = new Date();
+        const diff = newYear - currentTime;
 
-//         const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-//         const h = Math.floor(diff / 1000 / 60 / 60) % 24 ;
-//         const m = Math.floor(diff / 1000 / 60) % 60;
-//         const s = Math.floor(diff / 1000) % 60;
+        const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+        const h = Math.floor(diff / 1000 / 60 / 60) % 24 ;
+        const m = Math.floor(diff / 1000 / 60) % 60;
+        const s = Math.floor(diff / 1000) % 60;
 
-//         days.innerHTML = d;
-//         hours.innerHTML = h < 10 ? '0' + h : h; 
-//         minutes.innerHTML = m < 10 ? '0' + m : m; 
-//         seconds.innerHTML = s < 10 ? '0' + s : s; 
-// }
+        days.innerHTML = d;
+        hours.innerHTML = h < 10 ? '0' + h : h; 
+        minutes.innerHTML = m < 10 ? '0' + m : m; 
+        seconds.innerHTML = s < 10 ? '0' + s : s; 
 
-//     setInterval(updateCountdownTime, 1000);
-// }
+}
+
+    setInterval(updateCountdownTime, 1000);
+}
 
