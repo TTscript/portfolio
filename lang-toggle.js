e566e6bfe667c93f60cecf525e6a80066f3da38f
Timur <t.tokumov@gmail.com>
2022-02-17 01:28:12 +0600

    changed describe author

--- lang-toggle.js
+++ lang-toggle.js
@@ -13,8 +13,8 @@ export const langToggle = {
       ru: `Обо мне`,  
     },
     about: {
-        en: `My name is Timur. I'm from Kazakhstan. I recently completed HTML academy online course that lasted 9 months and I continue to evolve in this direction. Below you can see projects which I did.`,
-        ru: `Меня зовут Тимур. В настоящее время живу в Казахстане. Недавно закончил онлайн курсы HTML академии, которые длились около 9 месяцев и сейчас продолжаю развиваться в этом направлении. Ниже вы можете увидеть проекты, которыми я занимался.`,
+        en: `My name is Timur. I'm from Kazakhstan. Below you can see projects which I did.`,
+        ru: `Меня зовут Тимур. В настоящее время живу в Казахстане. Ниже вы можете увидеть проекты, которыми я занимался.`,
     },
     tel: {
         en: `tel`,
